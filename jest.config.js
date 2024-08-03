b5562541a554041169cdeb6afbfdb90f010cb85f
Tin <adduser@Shinya>
2024-08-04 03:34:45 +1000

    Separate Jest and Playwright tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  "testEnvironment": "node",
-  "coverageDirectory": "coverage",
-  "coverageReporters": ["json", "lcov", "text", "clover"]
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  collectCoverageFrom: [
+    "app.js",
+    "models/**/*.js",
+    "routes/**/*.js",
+    "!**/node_modules/**",
+  ],
+  testMatch: [
+    "**/tests/unit/**/*.test.js",
+    "**/tests/integration/**/*.test.js"
+  ]
 };
