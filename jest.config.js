72f77a6a039295c183cd5b6f593cc8fb7da2d51b
Tin <adduser@Shinya>
2024-08-04 16:20:00 +1000

    fix linting

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/tests/e2e/"],
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text-summary']
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text-summary"]
 };
