475c018663723b738597a7fad412fa67cccc84e9
Tin <adduser@Shinya>
2024-08-10 01:22:38 +1000

    Fix environment variable loading and configure Express views path

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 const Note = require('./models/note');
 const notesRouter = require('./routes/notes');
 const methodOverride = require('method-override');
-require('dotenv').config();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
@@ -19,7 +21,9 @@ app.get('/', async (req, res) => {
 mongoose.connect(process.env.SERVER, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Could not connect to MongoDB", err));
 
 app.use('/', notesRouter);
 app.listen(process.env.PORT || 3000, () => {
