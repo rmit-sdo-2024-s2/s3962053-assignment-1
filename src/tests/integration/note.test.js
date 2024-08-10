f0eba1fb3d7fd7f95cbf0360d447e02135837664
Tin <adduser@Shinya>
2024-08-10 13:49:10 +1000

    Enhance integration tests for Note API

--- src/tests/integration/note.test.js
+++ src/tests/integration/note.test.js
@@ -1,21 +1,106 @@
-const app = require('../../app')
-const supertest = require('supertest')
-const req = supertest(app)
-const Note = require("../../models/note")
+const app = require('../../app');
+const supertest = require('supertest');
+const req = supertest(app);
+const Note = require("../../models/note");
 
-// Integration Tests programmatically test the API used by the application
+describe('Note API Integration Tests', () => {
 
-describe('Add Note', () => {
-    it('Should add a new note and redirect to login', async () => {
+    beforeEach(async () => {
+        // Clear the database before each test
+        await Note.deleteMany({});
+    });
 
-        const res = await req
-            .post('/')
-            .send({
-                title: "[INTEGRATION TEST] New Note",
-                description: "This note was created at " + new Date(),
-            })
+    describe('Add Note', () => {
+        it('Should add a new note and redirect to homepage', async () => {
+            const res = await req
+                .post('/notes')
+                .send({
+                    title: "[INTEGRATION TEST] New Note",
+                    content: "This note was created at " + new Date(),
+                });
 
-        expect(res.statusCode).toEqual(302)
-        expect(res.headers['location']).toEqual('/')
-    })
-})
+            expect(res.statusCode).toEqual(302);
+            expect(res.headers['location']).toEqual('/');
+        });
+
+        it('Should not add a note without a title', async () => {
+            const res = await req
+                .post('/notes')
+                .send({
+                    content: "This note is missing a title",
+                });
+
+            expect(res.statusCode).toEqual(400);
+            expect(res.body.errors).toBeDefined();
+        });
+
+        it('Should not add a note without content', async () => {
+            const res = await req
+                .post('/notes')
+                .send({
+                    title: "[INTEGRATION TEST] Note without content",
+                });
+
+            expect(res.statusCode).toEqual(400);
+            expect(res.body.errors).toBeDefined();
+        });
+    });
+
+    describe('Toggle Note Importance', () => {
+        it('Should toggle the importance of a note', async () => {
+            const note = new Note({
+                title: "Note to be toggled",
+                content: "This note's importance will be toggled",
+                isImportant: false
+            });
+            await note.save();
+
+            const res = await req
+                .post(`/notes/${note._id}/important`)
+                .send();
+
+            expect(res.statusCode).toEqual(302);
+            expect(res.headers['location']).toEqual('/');
+
+            // Check if the note's importance has been toggled
+            const updatedNote = await Note.findById(note._id);
+            expect(updatedNote.isImportant).toBe(true);
+        });
+    });
+
+    describe('Delete Note', () => {
+        it('Should delete a note and redirect to homepage', async () => {
+            const note = new Note({
+                title: "Note to be deleted",
+                content: "This note will be deleted"
+            });
+            await note.save();
+
+            const res = await req
+                .delete(`/${note._id}`)
+                .send();
+
+            expect(res.statusCode).toEqual(302);
+            expect(res.headers['location']).toEqual('/');
+
+            // Check if the note has been deleted
+            const deletedNote = await Note.findById(note._id);
+            expect(deletedNote).toBeNull();
+        });
+    });
+
+    describe('Get Notes', () => {
+        it('Should get all notes', async () => {
+            await Note.create([
+                { title: "Note 1", content: "Content for note 1" },
+                { title: "Note 2", content: "Content for note 2" }
+            ]);
+
+            const res = await req.get('/');
+
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toBeDefined();
+            expect(res.body.length).toBe(2);
+        });
+    });
+});
